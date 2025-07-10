5e74a4d22bf91d2d54fb37d28106f87a1922f36c
aashnashahi <143892664+aashnashahi@users.noreply.github.com>
2025-07-10 22:54:00 +0530

    Update app.py

--- services/user/app.py
+++ services/user/app.py
@@ -1,19 +1,22 @@
-from flask import Flask, Response
-from prometheus_client import Counter, generate_latest, CONTENT_TYPE_LATEST
+from flask import Flask, jsonify, Response
+from prometheus_client import Counter, generate_latest
 
 app = Flask(__name__)
 
-# Prometheus metric for counting requests
-REQUEST_COUNTER = Counter('user_requests_total', 'Total HTTP Requests to User Service')
+# Prometheus metrics
+REQUEST_COUNT = Counter('user_http_requests_total', 'Total HTTP requests to User service')
+
+@app.before_request
+def before_request():
+    REQUEST_COUNT.inc()
 
 @app.route('/')
 def home():
-    REQUEST_COUNTER.inc()
-    return "Welcome to the User Service!"
+    return jsonify({'message': 'Welcome to the User Service'})
 
 @app.route('/metrics')
 def metrics():
-    return Response(generate_latest(), mimetype=CONTENT_TYPE_LATEST)
+    return Response(generate_latest(), mimetype='text/plain')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5002)
+    app.run(host='0.0.0.0', port=5000)
