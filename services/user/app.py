dadaa0020ebc3df8806a51c670f46ac8d94bd3b9
aashnashahi <143892664+aashnashahi@users.noreply.github.com>
2025-07-10 23:01:44 +0530

    Update app.py

--- services/user/app.py
+++ services/user/app.py
@@ -1,22 +1,18 @@
-from flask import Flask, jsonify, Response
+from flask import Flask, Response, jsonify
 from prometheus_client import Counter, generate_latest
 
 app = Flask(__name__)
 
-# Prometheus metrics
-REQUEST_COUNT = Counter('user_http_requests_total', 'Total HTTP requests to User service')
-
-@app.before_request
-def before_request():
-    REQUEST_COUNT.inc()
+REQUEST_COUNT = Counter('user_http_requests_total', 'Total HTTP Requests for User Service')
 
 @app.route('/')
 def home():
-    return jsonify({'message': 'Welcome to the User Service'})
+    REQUEST_COUNT.inc()
+    return jsonify(message="Welcome to the User Service!")
 
 @app.route('/metrics')
 def metrics():
     return Response(generate_latest(), mimetype='text/plain')
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
