2b34d4ff4df1b38133df6c0a47567c30c307c111
aashnashahi <143892664+aashnashahi@users.noreply.github.com>
2025-07-10 22:28:41 +0530

    Update app.py

--- services/booking/app.py
+++ services/booking/app.py
@@ -1,9 +1,19 @@
-from flask import Flask
+from flask import Flask, Response
+from prometheus_client import Counter, generate_latest, CONTENT_TYPE_LATEST
+
 app = Flask(__name__)
+
+# Define Prometheus metric
+REQUEST_COUNTER = Counter('booking_requests_total', 'Total HTTP Requests to Booking Service')
 
 @app.route('/')
 def home():
-    return "Booking Service Running!"
+    REQUEST_COUNTER.inc()
+    return "Welcome to the Booking Service!"
+
+@app.route('/metrics')
+def metrics():
+    return Response(generate_latest(), mimetype=CONTENT_TYPE_LATEST)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5002)
