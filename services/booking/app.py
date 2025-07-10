5ab36055ab22bc884d2768316d5bc74e58702437
aashnashahi <143892664+aashnashahi@users.noreply.github.com>
2025-07-10 23:39:19 +0530

    Update app.py

--- services/booking/app.py
+++ services/booking/app.py
@@ -4,21 +4,21 @@ from prometheus_client import Counter, generate_latest, CONTENT_TYPE_LATEST
 app = Flask(__name__)
 
 
-SERVICE_NAME = "booking"  
+SERVICE_NAME = "booking"
 
 REQUEST_COUNT = Counter(
-    f"{SERVICE_NAME}_http_requests_total", f"Total HTTP requests for {SERVICE_NAME} service"
+    f"{booking}_http_requests_total", f"Total HTTP requests for {booking} service"
 )
 
-@app.route('/')
+@app.route("/")
 def home():
     REQUEST_COUNT.inc()
-    return jsonify(message=f"Welcome to the {SERVICE_NAME.capitalize()} Service!")
+    return jsonify(message=f"Welcome to the {booking.capitalize()} Service!")
 
-@app.route('/metrics')
+@app.route("/metrics")
 def metrics():
     return Response(generate_latest(), mimetype=CONTENT_TYPE_LATEST)
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
 
