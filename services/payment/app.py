4b8c786af1c4591bfe263c50d9339595fd57bb9d
aashnashahi <143892664+aashnashahi@users.noreply.github.com>
2025-07-11 00:41:55 +0530

    Update app.py

--- services/payment/app.py
+++ services/payment/app.py
@@ -1,23 +1,19 @@
 from flask import Flask, Response, jsonify
-from prometheus_client import Counter, generate_latest, CONTENT_TYPE_LATEST
+from prometheus_client import Counter, generate_latest
 
 app = Flask(__name__)
 
+# Prometheus Counter metric
+REQUEST_COUNT = Counter('payment_http_requests_total', 'Total HTTP Requests for Payment Service')
 
-SERVICE_NAME = "payment"
-
-REQUEST_COUNT = Counter(
-    f"{payment}_http_requests_total", f"Total HTTP requests for {payment} service"
-)
-
-@app.route("/")
+@app.route('/')
 def home():
     REQUEST_COUNT.inc()
-    return jsonify(message=f"Welcome to the {payment.capitalize()} Service!")
+    return jsonify(message="Welcome to the Payment Service!")
 
-@app.route("/metrics")
+@app.route('/metrics')
 def metrics():
-    return Response(generate_latest(), mimetype=CONTENT_TYPE_LATEST)
+    return Response(generate_latest(), mimetype='text/plain')
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
