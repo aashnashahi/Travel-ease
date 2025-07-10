937089bf5b1567b5d081041fb61ef42385199fe8
aashnashahi <143892664+aashnashahi@users.noreply.github.com>
2025-07-10 23:40:21 +0530

    Update app.py

--- services/payment/app.py
+++ services/payment/app.py
@@ -4,20 +4,20 @@ from prometheus_client import Counter, generate_latest, CONTENT_TYPE_LATEST
 app = Flask(__name__)
 
 
-SERVICE_NAME = "payment"  
+SERVICE_NAME = "payment"
 
 REQUEST_COUNT = Counter(
-    f"{SERVICE_NAME}_http_requests_total", f"Total HTTP requests for {SERVICE_NAME} service"
+    f"{payment}_http_requests_total", f"Total HTTP requests for {payment} service"
 )
 
-@app.route('/')
+@app.route("/")
 def home():
     REQUEST_COUNT.inc()
-    return jsonify(message=f"Welcome to the {SERVICE_NAME.capitalize()} Service!")
+    return jsonify(message=f"Welcome to the {payment.capitalize()} Service!")
 
-@app.route('/metrics')
+@app.route("/metrics")
 def metrics():
     return Response(generate_latest(), mimetype=CONTENT_TYPE_LATEST)
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
