610c1cfe8a7bfee0880bcfede08f804e06ae3ee7
aashnashahi <143892664+aashnashahi@users.noreply.github.com>
2025-07-10 22:28:07 +0530

    Update app.py

--- services/payment/app.py
+++ services/payment/app.py
@@ -1,9 +1,19 @@
-from flask import Flask
+from flask import Flask, Response
+from prometheus_client import Counter, generate_latest, CONTENT_TYPE_LATEST
+
 app = Flask(__name__)
+
+# Define Prometheus metric
+REQUEST_COUNTER = Counter('payment_requests_total', 'Total HTTP Requests to Payment Service')
 
 @app.route('/')
 def home():
-    return "Payment Service Running!"
+    REQUEST_COUNTER.inc()
+    return "Welcome to the Payment Service!"
+
+@app.route('/metrics')
+def metrics():
+    return Response(generate_latest(), mimetype=CONTENT_TYPE_LATEST)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5001)
