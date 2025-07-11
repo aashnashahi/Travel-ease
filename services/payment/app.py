e28e1bde3a871a63d5de51aa2d25cdb95f2a6145
aashnashahi <143892664+aashnashahi@users.noreply.github.com>
2025-07-11 10:22:14 +0530

    Update app.py

--- services/payment/app.py
+++ services/payment/app.py
@@ -6,16 +6,16 @@ app = Flask(__name__)
 # Prometheus metric
 REQUEST_COUNT = Counter('http_requests_total', 'Total HTTP Requests', ['service'])
 
-SERVICE_NAME = 'payment'  
+SERVICE_NAME = 'Payment'  
 
 @app.route('/')
 def home():
-    REQUEST_COUNT.labels(service=payment).inc()
-    return jsonify(message=f"Welcome to the {payment.capitalize()} Service!")
+    REQUEST_COUNT.labels(service=SERVICE_NAME).inc()
+    return jsonify(message=f"Welcome to the {SERVICE_NAME.capitalize()} Service!")
 
 @app.route('/metrics')
 def metrics():
     return Response(generate_latest(), mimetype='text/plain')
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5001)
